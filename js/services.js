38542b9e606693ca06b58801e10ad33be0f3e3c4
Krishna Kushwaha <kushwaha.krishna1233@gmail.com>
2024-11-20 00:09:02 +0530

    added many things completed bsaic with navbar

--- js/services.js
+++ js/services.js
@@ -95,16 +95,3 @@ const setMobileFriendly = () => {
         gsap.set(".menu__item-image_wrapper", { maxWidth: "300px", maxHeight: "300px" }); // Smaller images
     }
 };
-
-window.addEventListener("resize", setMobileFriendly);
-setMobileFriendly();
-
-const slider = document.querySelector('.slider');
-
-function activate(e) {
-  const items = document.querySelectorAll('.item');
-  e.target.matches('.next') && slider.append(items[0])
-  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
-}
-
-document.addEventListener('click',activate,false);
